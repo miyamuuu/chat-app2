7660d8fb1031273e3c35027e081d6a0d3e4ff060
miyamuuu <miyamuuu386@gmail.com>
2020-10-07 07:02:41 +0900

    各モデルのテスト

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :model do
+  describe '#create' do
+    before do
+      @user = FactoryBot.build(:user)
+    end
+
+    it "重複したemailが存在する場合登録できないこと" do
+      @user.save
+      another_user = FactoryBot.build(:user, email: @user.email)
+      another_user.valid?
+      expect(another_user.errors.full_messages).to include("Email has already been taken")
+
+    end
+  end
+end
